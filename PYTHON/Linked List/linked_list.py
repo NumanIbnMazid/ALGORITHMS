864caf72195d93026c9f3a87c2ed5a5e2a5cc5e8
NumanIbnMazid <numanibnmazid@gmail.com>
2021-11-30 17:29:55 +0600

    initialized: linked list

---
+++ PYTHON/Linked List/linked_list.py
@@ -0,0 +1,9 @@
+"""
+Keywords: Linear Data Structure, Series of connected nodes
+Notes:
+    - Each node stores data and the address of the next node.
+Types of Linked List:
+    * Singly Linked List
+    * Doubly Linked List
+    * Circular Linked List
+"""
