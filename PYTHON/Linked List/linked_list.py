2548ea7066ce4e6c3d544c2bf7f9a4e5185c37d8
NumanIbnMazid <numanibnmazid@gmail.com>
2021-11-30 17:55:24 +0600

    completed: Linked List

--- PYTHON/Linked List/linked_list.py
+++ PYTHON/Linked List/linked_list.py
@@ -7,3 +7,32 @@ Types of Linked List:
     * Doubly Linked List
     * Circular Linked List
 """
+
+# Simple Linked List
+
+class Node():
+    # Creating a node
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+        
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    
+    # Assigning item values
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    
+    # Connecting Nodes
+    linked_list.head.next = second
+    second.next = third
+    
+    # Print the items in linked list
+    while linked_list.head != None:
+        print(linked_list.head.item)
+        linked_list.head = linked_list.head.next
