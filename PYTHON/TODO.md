d12bb914640687a21419b2b90030d2ef94ee0338
NumanIbnMazid <numanibnmazid@gmail.com>
2021-12-04 13:34:50 +0600

    completed: Tree

--- PYTHON/TODO.md
+++ PYTHON/TODO.md
@@ -2,6 +2,8 @@
 
 * Implement Priority Queue using Max Heap / Min Heap
 
+* Fibonacci Heap + Fibonacci Heap Operations
+
 ## Next
 
 * <https://www.programiz.com/dsa/linked-list>
---
+++ PYTHON/Tree/tree.py
@@ -0,0 +1,17 @@
+"""
+Keywords: Non-linear Data Structure, Connected Nodes, Connected by Edges
+Terminologies:
+    * Node: An entity that contains a key or value and pointers to it's child nodes. The last nodes of each path are called leaf nodes or external nodes that do not contain a link/pointer to child nodes. The node having at least a child node is called an internal node.
+    * Edge: Link between any two nodes.
+    * Root: Top most node of a tree.
+    * Height of a Node: The height of a node is the number of edges from the node to the deepest leaf (ie. the longest path from the node to a leaf node)
+    * Depth of a node: The deepth of a node the number of edges from the root to the node.
+    * Height of a tree: The height of a tree is the height of the root node or the deepth of the deepest node.
+    * Degree of a node: The degree of a node is the total number of branches of that node.
+    * Forest: A collection of disjoint trees is called a forest.
+Types of Trees:
+    * Binary Tree
+    * Binary Search Tree
+    * AVL Tree
+    * B-Tree
+"""
