096c69c446fef1100246421f6498ac62de74b57e
NumanIbnMazid <numanibnmazid@gmail.com>
2021-12-05 11:50:30 +0600

    completed: Full Binary Tree

--- PYTHON/Tree/full_binary_tree.py
+++ PYTHON/Tree/full_binary_tree.py
@@ -23,3 +23,37 @@ Full Binary Tree Theorems:
     * The number of leaves at most:
         -> 2^λ - 1
 """
+
+# creating node
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.rightChild = None
+        self.leftChild = None
+        
+    
+# Checking full binary tree
+def isFullTree(root):
+    # tree empty case
+    if root is None:
+        return True
+    # checking if child is present
+    if root.leftChild is None and root.rightChild is None:
+        return True
+    if root.leftChild is not None and root.rightChild is not None:
+        return (isFullTree(root.leftChild) and isFullTree(root.rightChild))
+    
+
+root = Node(1)
+root.rightChild = Node(3)
+root.leftChild = Node(2)
+
+root.leftChild.leftChild = Node(4)
+root.leftChild.rightChild = Node(5)
+root.leftChild.rightChild.leftChild = Node(6)
+root.leftChild.rightChild.rightChild = Node(7)
+
+if isFullTree(root):
+    print("The tree is a full binary tree")
+else:
+    print("The tree is not a full binary tree")
