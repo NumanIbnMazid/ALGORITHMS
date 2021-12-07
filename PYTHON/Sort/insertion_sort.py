13d30a8bf28dbecd85d6de99c250f29a3d18ca75
NumanIbnMazid <numanibnmazid@gmail.com>
2021-12-07 18:20:01 +0600

    updated

---
+++ PYTHON/Sort/insertion_sort.py
@@ -0,0 +1,11 @@
+"""
+Notes:
+    - Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration.
+    - Insertion sort works similarly as we sort cards in our hand in a card game.
+Time Complexity:
+    - Best	O(n)
+    - Worst	O(n2)
+    - Average	O(n2)
+Space Complexity: O(1)
+"""
+
--- PYTHON/test.py
+++ PYTHON/test.py
@@ -39,8 +39,81 @@
 # queue = collections.deque([1, 3, 4, 5])
 # print(queue)
 
-myArr = [1, 2, 3, 4, 5, 6, 7]
+# myArr = [1, 2, 3, 4, 5, 6, 7]
 
-for i in range(0, len(myArr)):
-    for j in range(0, len(myArr) - i - 1):
-        print(myArr[i], myArr[j])
+# for i in range(0, len(myArr)):
+#     for j in range(0, len(myArr) - i - 1):
+#         print(myArr[i], myArr[j])
+
+
+myMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+def get_result(matrix):
+    result = []
+
+    preRes = []
+
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            preRes.append([matrix[i], matrix[j]])
+    
+    def recursion(arr1, arr2):
+        for i in arr1:
+            pre = []
+            for j in arr2:
+                pre.append(i + " " + j)
+            result.append(pre)
+                
+    for arr in preRes:
+        recursion(arr1=arr[0], arr2=arr[-1])
+
+    return result
+
+
+myMatrix = [
+    ["Red", "Blue", "Black"],
+    ["A", "B", "C"],
+    ["1", "2", "3"]
+]
+
+# myMatrix = [
+#     ["Red", "Blue"],
+#     ["S", "M"]
+# ]
+
+print(get_result(myMatrix))
+
+# myMatrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+
+# display = [
+#     [4, 5, 6], 
+#     [8, 10, 12], 
+#     [12, 15, 18], 
+#     [7, 8, 9], 
+#     [14, 16, 18], 
+#     [21, 24, 27], 
+#     [28, 32, 36], 
+#     [35, 40, 45], 
+#     [42, 48, 54]
+# ]
+
+display = [
+    ['Red A', 'Red B', 'Red C'], 
+    ['Blue A', 'Blue B', 'Blue C'], 
+    ['Black A', 'Black B', 'Black C'], 
+    ['Red 1', 'Red 2', 'Red 3'], 
+    ['Blue 1', 'Blue 2', 'Blue 3'], 
+    ['Black 1', 'Black 2', 'Black 3'], 
+    ['A 1', 'A 2', 'A 3'], 
+    ['B 1', 'B 2', 'B 3'], 
+    ['C 1', 'C 2', 'C 3']
+]
